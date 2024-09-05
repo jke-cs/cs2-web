57f77098a33beb8d2ab3c768b757254d572771d0
Krigsgaldr793 <koestler.johannes1997@gmail.com>
2024-09-05 13:07:17 +0200

    Update route.js

--- cs2-web/app/api/route.js
+++ cs2-web/app/api/route.js
@@ -1,7 +1,6 @@
 import mysql from 'mysql2/promise';
 
 export async function GET() {
-  // Erstelle eine Verbindung zur Datenbank
   const connection = await mysql.createConnection({
     host: '',
     user: '',
@@ -11,17 +10,13 @@ export async function GET() {
   });
 
   try {
-    // Führe die Abfrage aus, um alle Einträge der Tabelle PlayerRecords abzurufen
     const [rows] = await connection.query('SELECT * FROM PlayerRecords');
-    // Beende die Verbindung
     await connection.end();
     
-    // Sende die Daten als JSON-Antwort
     return new Response(JSON.stringify(rows), {
       headers: { 'Content-Type': 'application/json' },
     });
   } catch (error) {
-    // Behandle Fehler und sende eine Fehlermeldung
     return new Response(JSON.stringify({ error: 'Failed to fetch data' }), {
       status: 500,
       headers: { 'Content-Type': 'application/json' },
