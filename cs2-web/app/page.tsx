1010fb718211775ede1aefeb93d74ce94224b958
Krigsgaldr793 <koestler.johannes1997@gmail.com>
2024-09-06 16:34:17 +0200

    Update page.tsx
    Credits to Boogot for integrating the Leaderboard

--- cs2-web/app/page.tsx
+++ cs2-web/app/page.tsx
@@ -1,9 +1,8 @@
 "use client";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { FaCopy } from 'react-icons/fa';
-import { useEffect } from 'react';
+import { FaCopy, FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 import axios from 'axios';
 
 interface PlayerData {
@@ -12,7 +11,8 @@ interface PlayerData {
   GlobalPoints: number;
 }
 function closeHeader() {
-  document.getElementById('header').style.display = 'none';
+  const header = document.getElementById('header');
+  if (header) header.style['display'] = 'none';
 }
 
 export default function Home() {
@@ -21,11 +21,38 @@ export default function Home() {
   const [activeSection, setActiveSection] = useState<string>('home');
   const [tableData, setTableData] = useState<any[]>([]);
 
+  const [players, setPlayers] = useState<PlayerData[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const playersPerPage = 10;
+
+  useEffect(() => {
+    const fetchPlayers = async () => {
+      try {
+        const response = await fetch('/api/getPlayers');
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        setPlayers(data);
+      } catch (error) {
+        console.error('Error fetching players:', error);
+      }
+    };
+
+    fetchPlayers();
+  }, []);
 
   const handleCopy = (text: string) => {
     navigator.clipboard.writeText(text);
     alert('Copied to clipboard!');
   };
+
+  const indexOfLastPlayer = currentPage * playersPerPage;
+  const indexOfFirstPlayer = indexOfLastPlayer - playersPerPage;
+  const currentPlayers = players.slice(indexOfFirstPlayer, indexOfLastPlayer);
+
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
   return (
     <div className="flex relative min-h-screen overflow-hidden">
       <div className="absolute top-0 left-0 w-full h-full bg-[url('/bg-cbble.svg')] bg-cover bg-center z-[-1]"></div>
@@ -58,13 +85,7 @@ export default function Home() {
     50€ giveaway for the player with the most global points in one month. Join the <a href="https://discord.gg/n4xCDWrQRB" className="text-white underline">Discord</a> for more infos.
   </span>
 </header>
-
-
-
-
-
-
-
+ 
 
       {/* Navigationsleiste */}
       <nav className="fixed top-0 left-0 h-full w-16 bg-gray-900 flex flex-col items-center py-4">
@@ -161,13 +182,16 @@ export default function Home() {
       </nav>
 
       <main className="flex-1 min-h-screen flex flex-col items-center justify-start p-24 ml-16 relative z-10">
+
       
 
   <div className="relative z-[-1] flex place-items-center before:absolute before:h-[10px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-0 after:h-[100px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]"></div>
+  <div className="relative z-[-150] flex place-items-center before:absolute before:bottom-[-800px] before:left-[-600px] before:h-[20px] before:w-full before:rounded-full before:bg-gradient-radial before:from-purple-700 before:to-transparent before:blur-3xl before:opacity-80 before:content-[''] after:absolute after:bottom-[-300px] after:left-[-600px] after:-z-0 after:h-[200px] after:w-full after:bg-gradient-conic after:from-purple-500 after:via-purple-700 after:blur-3xl after:opacity-20 after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-purple-900 before:dark:opacity-20 after:dark:from-purple-800 after:dark:via-purple-600 after:dark:opacity-12 sm:before:w-[600px] sm:after:w-[300px] before:lg:h-[400px]"></div>
 
 
  <div className="w-full max-w-6xl grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
     {activeSection === 'home' && (
+ 
       <>
               <a
                 className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -308,6 +332,7 @@ export default function Home() {
                   src="/surf_boreas.svg"
                   alt="Surf Boreas"
                   width={400} 
+                  height={100} 
                   className="mb-3"
                 />
                 <p className="m-0 max-w-[30ch] text-sm opacity-50">
@@ -324,18 +349,7 @@ export default function Home() {
               </a>
             </>
           )}
-        {/* Discord Widget */}
-<div className="absolute top-24 left-0 ml-8">
 
-<iframe 
-  src="https://discord.com/widget?id=371718546121556002&theme=dark" 
-  width="350" 
-  height="500" 
-  allowTransparency={true} 
-  frameBorder="0" 
-  sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts">
-</iframe>
-</div>
  
 <div className="flex flex-col space-y-4 absolute right-8 top-24">
 <div className="bg-gray-800 text-white p-6 rounded-lg shadow-lg max-w-xs z-20 opacity-50">
@@ -381,7 +395,6 @@ export default function Home() {
 </div>
   </div>
 
-<div className="relative z-[-50] flex place-items-center before:absolute before:bottom-[-300px] before:left-[-600px] before:h-[20px] before:w-full before:rounded-full before:bg-gradient-radial before:from-purple-700 before:to-transparent before:blur-3xl before:opacity-80 before:content-[''] after:absolute after:bottom-[-300px] after:left-[-600px] after:-z-0 after:h-[200px] after:w-full after:bg-gradient-conic after:from-purple-500 after:via-purple-700 after:blur-3xl after:opacity-20 after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-purple-900 before:dark:opacity-20 after:dark:from-purple-800 after:dark:via-purple-600 after:dark:opacity-12 sm:before:w-[600px] sm:after:w-[300px] before:lg:h-[400px]"></div>
 
 
 
@@ -409,10 +422,55 @@ export default function Home() {
       Map: skins
     </p>
   </a>
+
+
 )}
   
 {activeSection === 'best' && (
-  <div className="overflow-x-auto">
+  <div className="container mx-auto px-4 py-8 ml-[-315px]"> {/* Zentriert den Inhalt */}
+    <div className="mx-auto bg-gray-800 shadow-lg rounded-lg overflow-hidden" style={{ width: '1000px' }}> {/* Feste Breite von 1000px und zentriert */}
+      <table className="w-full table-fixed divide-y divide-gray-700">
+        <thead className="bg-gray-900">
+          <tr>
+            <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">Rank</th>
+            <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">Player Name</th>
+            <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">Global Points</th>
+          </tr>
+        </thead>
+        <tbody className="bg-gray-800 divide-y divide-gray-700">
+          {currentPlayers.map((player, index) => (
+            <tr key={player.SteamID} className="hover:bg-gray-700 transition-colors">
+              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-300">{indexOfFirstPlayer + index + 1}</td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-300">{player.PlayerName}</td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-300">
+                <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                  {player.GlobalPoints}
+                </span>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+    <div className="mt-4 flex justify-between items-center" style={{ width: '1000px', margin: '0 auto' }}> {/* Zentriert den Pagination-Container */}
+      <button
+        onClick={() => paginate(currentPage - 1)}
+        disabled={currentPage === 1}
+        className="px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 disabled:opacity-50"
+      >
+        <FaChevronLeft />
+      </button>
+      <span className="text-sm text-gray-700">
+        Page {currentPage} of {Math.ceil(players.length / playersPerPage)}
+      </span>
+      <button
+        onClick={() => paginate(currentPage + 1)}
+        disabled={currentPage === Math.ceil(players.length / playersPerPage)}
+        className="px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 disabled:opacity-50"
+      >
+        <FaChevronRight />
+      </button>
+    </div>
   </div>
 )}
 
@@ -534,6 +592,7 @@ export default function Home() {
         <footer className="w-full text-center py-2 bg-[rgba(255,255,255,0.8)] text-black absolute bottom-0 left-0">
   <p>This website was created by JKE (c)</p>
 </footer>
+
 
 
       </main>
